838be1383a3e7753ded55657219227e0344d3c39
LCS <LCS@ANASBURWAIS>
2024-07-05 00:30:20 +0300

    Add project files.

---
+++ CascadingTask.Domain/User.cs
@@ -0,0 +1,14 @@
+﻿namespace CascadingTask.Domain
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+}
+
+
+  
+
+
